81b2abeecaf738b81be68fb856e353bdbe873410
hecrean <hector@hectorcrean.xyz>
2022-10-27 15:45:23 +0100

    init media-lib service

---
+++ crates/media_library/tests/health_check.rs
@@ -0,0 +1,5 @@
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
